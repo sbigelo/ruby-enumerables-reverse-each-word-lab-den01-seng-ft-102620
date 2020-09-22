7bc32724b45673b869b3bffa90acf8afd7bc35f4
Sean Bigelow <sean.bigelow1@gmail.com>
2020-09-22 16:56:08 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,9 @@
+require 'pry'
+
+
 def reverse_each_word(sentence)
-sentence.split.collect {|sentence| sentence.reverse}.join(" ")
+sentence.split.collect {|i| i.reverse}.join(" ")
+
 end
- 
+# sentence.split.reverse.join(" ").reverse
+# "iH ,niaga tsuj gnikam erus s'ti !desrever"
